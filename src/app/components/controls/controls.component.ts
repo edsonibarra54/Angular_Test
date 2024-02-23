1fae94df63c91eed1b70e9feda13382341466f6f
Edson Ibarra <edsonibarra54@gmail.com>
2024-02-22 18:08:13 -0600

    Added a button to invert the list of the cards

--- src/app/components/controls/controls.component.ts
+++ src/app/components/controls/controls.component.ts
@@ -10,6 +10,8 @@ import { Component, EventEmitter, Output } from '@angular/core';
 export class ControlsComponent {
   @Output()
   public evento: EventEmitter<boolean> = new EventEmitter();
+  @Output()
+  public invertir: EventEmitter<void> = new EventEmitter();
 
   public onClickSetAll() : void{
     console.log("Click en seleccionar todos");
@@ -19,5 +21,10 @@ export class ControlsComponent {
   public onClickUnsetAll() : void{
     console.log("Click en seleccionar todos");
     this.evento.emit(false);
+  }
+
+  public onClickInvert() : void{
+    console.log("Click en invertir tarjetas");
+    this.invertir.emit();
   }
 }
